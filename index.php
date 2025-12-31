ca53668cd2761b2a15ea2dcc67f9b9b03fea53fb
NitSanghani111 <nitsanghani05@gmail.com>
2025-12-31 22:32:36 +0530

    ci/cd fix

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/db.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Fetch top 6 blogs
 $sql = "SELECT b.*, GROUP_CONCAT(c.name SEPARATOR '|') AS categories
         FROM blog b
